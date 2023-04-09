afa6930b9010ccd00bd4cb8f3dff3133aa1e1015
Spich <spichakovda@gmail.com>
2023-04-09 23:36:33 +0300

    Initial Commit

---
+++ SearchPlaceApp/SearchPlaceAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SearchPlaceAppApp.swift
+//  SearchPlaceApp
+//
+//  Created by Дмитрий Спичаков on 09.04.2023.
+//
+
+import SwiftUI
+
+@main
+struct SearchPlaceAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
